0648e4832c4e180a2b853437529dd630ee1b6be4
ncfex <oguzhanguldibi@gmail.com>
2024-10-16 16:39:08 +0300

    add RPC defs

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -22,8 +24,21 @@ type ExampleReply struct {
 	Y int
 }
 
-// Add your RPC definitions here.
+type GetTaskArgs struct{}
 
+type GetTaskReply struct {
+	TaskType string
+	TaskID   int
+	File     string
+	NReduce  int
+}
+
+type TaskCompletedArgs struct {
+	TaskType string
+	TaskID   int
+}
+
+type TaskCompletedReply struct{}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
